c4ba117b7d0d3b72ce6d84920943024a795adeaa
Joao Moura <joaomdmoura@gmail.com>
2012-05-23 13:23:18 -0300

    improving view of shits

--- app/controllers/shits_controller.rb
+++ app/controllers/shits_controller.rb
@@ -1,7 +1,11 @@
 class ShitsController < ApplicationController
 
-	def index
-		@shits = Shit.page(params[:page]).per(6)
-	end
+  def index
+    @shits = Shit.order(:created_at).page(params[:page]).per(6)
+  end
+
+  def create
+    shit = Shit.new(params[:shit]).save!
+  end
 
 end
