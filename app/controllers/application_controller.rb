408caba5407033124857605e75623360469c33bb
Joao Moura <joaomdmoura@gmail.com>
2012-05-25 01:15:35 -0300

    fixing url in facebook integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def fb_oath_token
-    @oauth = Koala::Facebook::OAuth.new("368626389864429", "c43ca76ee324061d00ce7c434f59624f", "http://www.friendsshit.com")
+    @oauth = Koala::Facebook::OAuth.new("368626389864429", "c43ca76ee324061d00ce7c434f59624f", "http://friendsshit.com")
     p @oauth.get_access_token_info(params[:code]) if params[:code]
   end
 
