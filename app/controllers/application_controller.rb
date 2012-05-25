9444850a6bbc9b83535c8ac5261914fe277cf185
Joao Moura <joaomdmoura@gmail.com>
2012-05-25 01:56:19 -0300

    testing Facebook #6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     @oauth = Koala::Facebook::OAuth.new("368626389864429", "c43ca76ee324061d00ce7c434f59624f", "http://friendsshit.com/")
     if params[:code]
     	p @oauth.url_for_access_token(params[:code])
-    	p @oauth.get_access_token(params[:code])
+    	p @oauth.get_app_access_token
     end
   end
 
