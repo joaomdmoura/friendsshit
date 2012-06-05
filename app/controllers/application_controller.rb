d01e82bf8433dcc8f32a9d840af441f5bd0fb86f
root <root@D3TR1.(none)>
2012-06-05 04:56:17 +0000

    rolling abck application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,16 @@ class ApplicationController < ActionController::Base
   def fb_oath_token
     @oauth = Koala::Facebook::OAuth.new("368626389864429", "c43ca76ee324061d00ce7c434f59624f", "http://friendsshit.com/")
     if params[:code] || params[:fb]
-      access_token = @oauth.get_app_access_token
-      @user_fid = @oauth.get_user_from_cookies(cookies)
-      @graph    = Koala::Facebook::API.new(access_token)
+      code = ( params[:code] ) ? params[:code] : params[:fb][:code]
+      access_token = @oauth.get_access_token(code)
+      @graph = Koala::Facebook::API.new(access_token)
       return true
     end
     return false
   end
 
   def get_fb_friends
-    @fb_friends = @graph.get_connections(@user_fid, "friends")
+    @fb_friends = @graph.get_connections("me", "friends")
   end
 
 end
