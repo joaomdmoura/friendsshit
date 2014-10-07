4977a06e6bdc4905765a64748b62f027ef881be5
Joao Moura <joaomdmoura@gmail.com>
2014-10-07 02:49:44 -0300

    removing regex validation

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -3,11 +3,8 @@ class Friend < ActiveRecord::Base
   attr_accessible :email, :fb_ib, :image, :name, :photo_url
   image_accessor :photo
 
-  EmailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  NameFormat = /^[A-ZÀ-ÿ\s-]+$/i
-
-  validates :email, :uniqueness => true, :format => {:with => EmailFormat}, :allow_nil => true
-  validates :name, :format => { :with => NameFormat }
+  validates :email, :uniqueness => true, :allow_nil => true
+  validates :name, :presence
 
   belongs_to :user
   belongs_to :shit
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :fb_token, :name
 
-  EmailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
-  NameFormat = /^[A-ZÀ-ÿ\s-]+$/i
-
-  validates :email, :uniqueness => true, :format => {:with => EmailFormat}
-  validates :name, :format => { :with => NameFormat }
+  validates :email, :uniqueness => true
+  validates :name, :presence
 
   has_many :shits , :through => :friends
   has_many :friends
