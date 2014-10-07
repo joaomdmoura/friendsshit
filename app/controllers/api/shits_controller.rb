af54393813b66c57f51c840c19179ba6966353f4
Joao Moura <joaomdmoura@gmail.com>
2014-10-07 02:56:37 -0300

    updating urls to heroku

--- app/controllers/api/shits_controller.rb
+++ app/controllers/api/shits_controller.rb
@@ -1,7 +1,7 @@
 class Api::ShitsController < ApplicationController
 
   def get_shits
-    
+
     @shits = Shit.select("shits.*, friends.*").joins(:friends).order("shits.created_at DESC").page(params[:page]).per(params[:per_page])
     @shits.map! {|x| x[:photo_url] = Friend.find([x.id])[0].photo.thumb('120x122#').url; x }
     render :json => @shits
@@ -9,17 +9,17 @@ class Api::ShitsController < ApplicationController
   end
 
   def create_shit
-    
+
     params[:shit]   = { :phrase => params[:phrase], :location => (params[:location]) ? params[:location] : "" } if params[:shit].nil?
 
     params[:friend] = { :name => params[:name], :fb_ib => ( params[:fb_ib] ) ? params[:fb_ib] : "" , :photo_url => ( params[:photo_url] ) ? params[:photo_url] : "" } if params[:friend].nil?
 
     @shit = Shit.new( params[:shit] )
-    
 
-    params[:friend][:photo_url] = ( params[:friend][:photo_url] != "" ) ? params[:friend][:photo_url] : "http://www.friendscrap.com.br/assets/troll/#{ 1 + rand(12) }.png"
+
+    params[:friend][:photo_url] = ( params[:friend][:photo_url] != "" ) ? params[:friend][:photo_url] : "https://friendsquotes.herokuapp.com/assets/troll/#{ 1 + rand(12) }.png"
     params[:friend][:name] = ( params[:friend][:name] != "" ) ? params[:friend][:name] : "Anonymous"
-    @friend       = Friend.new( params[:friend] ) 
+    @friend       = Friend.new( params[:friend] )
     @friend.shit  = @shit
     @friend.save!
 
@@ -28,17 +28,17 @@ class Api::ShitsController < ApplicationController
     if params[:friend][:fb_ib] != ""
       fb_oath_token
       shit_obj = {  "name"        => "Friend's Shit - Cuz shit happens!",
-                    "link"        => "http://www.friendscrap.com.br/shit/#{@shit.id}",
+                    "link"        => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}",
                     "caption"     => "A place to share yout friends shit and get some fun of others shits!",
                     "description" => "'#{@shit.phrase} %>'- #{@shit.friends[0].name}",
                     "picture"     => @shit.friends[0].photo.thumb('160x160#').url}
 
-      @graph.put_wall_post("", {:link => "http://www.friendscrap.com.br/shit/#{@shit.id}"})
-      @graph.put_wall_post("", {:link => "http://www.friendscrap.com.br/shit/#{@shit.id}"}, "#{params[:friend][:fb_ib]}")
+      @graph.put_wall_post("", {:link => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}"})
+      @graph.put_wall_post("", {:link => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}"}, "#{params[:friend][:fb_ib]}")
     end
-    
+
     @shit[:friend] = @friend
-    
+
     render :json => @shit
 
   end
--- app/controllers/shits_controller.rb
+++ app/controllers/shits_controller.rb
@@ -4,27 +4,27 @@ class ShitsController < ApplicationController
     get_fb_friends if fb_oath_token
     @shits      = Shit.order("created_at DESC").page(params[:page]).per(6)
     page        = (params[:page]) ? params[:page].to_i : 1
-    @next_page  = (page >= (Shit.count().to_f / 6)) ? 1 : page + 1 
+    @next_page  = (page >= (Shit.count().to_f / 6)) ? 1 : page + 1
   end
 
   def create
     @shit = Shit.new( params[:shit] )
-    
-    params[:friend][:photo_url] = ( params[:friend][:photo_url] != "" ) ? params[:friend][:photo_url] : "http://www.friendscrap.com.br/assets/troll/#{ 1 + rand(12) }.png"
+
+    params[:friend][:photo_url] = ( params[:friend][:photo_url] != "" ) ? params[:friend][:photo_url] : "https://friendsquotes.herokuapp.com/assets/troll/#{ 1 + rand(12) }.png"
     params[:friend][:name] = ( params[:friend][:name] != "" ) ? params[:friend][:name] : "Anonymous"
-    @friend       = Friend.new( params[:friend] ) 
+    @friend       = Friend.new( params[:friend] )
     @friend.shit  = @shit
 
     if @shit.save! && @friend.save! && !@friend.fb_ib.nil?
       fb_oath_token
       shit_obj = {  "name"        => "Friend's Shit - Cuz shit happens!",
-                    "link"        => "http://www.friendscrap.com.br/shit/#{@shit.id}",
+                    "link"        => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}",
                     "caption"     => "A place to share yout friends shit and get some fun of others shits!",
                     "description" => "'#{@shit.phrase} %>'- #{@friend.name}",
                     "picture"     => @friend.photo.thumb('160x160#').url}
 
-      @graph.put_wall_post("", {:link => "http://www.friendscrap.com.br/shit/#{@shit.id}"})
-      @graph.put_wall_post("", {:link => "http://www.friendscrap.com.br/shit/#{@shit.id}"}, "#{params[:friend][:fb_ib]}")
+      @graph.put_wall_post("", {:link => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}"})
+      @graph.put_wall_post("", {:link => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}"}, "#{params[:friend][:fb_ib]}")
     end
 
     respond_to do |format|
