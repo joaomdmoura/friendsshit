ecbb49099471affa2896dc989e9cd936fc091d8e
Joao Moura <joaomdmoura@gmail.com>
2014-11-12 02:44:47 -0200

    removing graph defition

--- app/controllers/shits_controller.rb
+++ app/controllers/shits_controller.rb
@@ -22,7 +22,6 @@ class ShitsController < ApplicationController
                     "description" => "'#{@shit.phrase} %>'- #{@friend.name}",
                     "picture"     => @friend.photo.thumb('160x160#').url}
 
-      @graph = fb_oath_token
       @graph.put_wall_post("", {:link => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}"})
       @graph.put_wall_post("", {:link => "https://friendsquotes.herokuapp.com/shit/#{@shit.id}"}, "#{params[:friend][:fb_ib]}")
     end
